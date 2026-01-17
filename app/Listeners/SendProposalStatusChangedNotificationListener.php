7340f3f2a0160721ae6ea01c4ea47182e01604be
Jakob <hakob.grigoryan.1992@gmail.com>
2026-01-17 16:00:07 +0400

    add missing job

--- app/Listeners/SendProposalStatusChangedNotificationListener.php
+++ app/Listeners/SendProposalStatusChangedNotificationListener.php
@@ -6,12 +6,16 @@ namespace App\Listeners;
 
 use App\Events\ProposalStatusChanged;
 use App\Jobs\SendProposalStatusChangedNotificationJob;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
 /**
  * Listener for ProposalStatusChanged event.
  * Dispatches notification job to send email to speaker.
  */
-class SendProposalStatusChangedNotificationListener
+class SendProposalStatusChangedNotificationListener implements ShouldQueue
 {
+    use InteractsWithQueue;
 
     /**
      * Handle the event.
